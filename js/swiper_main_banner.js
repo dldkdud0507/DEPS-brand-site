2a7f42089b01df710e4cd77b09af45d772505c72
dldkdud0507 <dldkdud0507@naver.com>
2025-01-14 16:41:39 +0900

    main sec_05 finish

---
+++ js/swiper_main_banner.js
@@ -0,0 +1,22 @@
+let rollingSwiper; // Swiper 슬라이드
+
+// 롤링시작 함수
+function PlayRollingSwiper(target) {
+    rollingSwiper = new Swiper('.swiper-container', {
+        spaceBetween: 0,
+        centeredSlides: true,
+        speed: 6000,
+        autoplay: {
+            delay: 0,
+        },
+        loop: true,
+        slidesPerView: 'auto',
+        allowTouchMove: false,
+        disableOnInteraction: false,
+    });
+}
+
+// 페이지 로드
+window.addEventListener('load', function () {
+    PlayRollingSwiper();
+});
